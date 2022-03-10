127d7a04854beb0c6e442ac6e21ff9907cf4e85f
Alex <emerald2014@mail.ru>
2022-03-10 14:50:24 +0300

    Добавил иконку, колесо, очки

--- app/src/main/java/ru/kudesnik/aateam/SpinFragment.kt
+++ app/src/main/java/ru/kudesnik/aateam/SpinFragment.kt
@@ -1,21 +1,28 @@
 package ru.kudesnik.aateam
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ru.kudesnik.aateam.databinding.FragmentSpinBinding
 import java.util.*
 
 
 class SpinFragment : Fragment() {
-    val sRandom: Random = Random()
+    private var _binding: FragmentSpinBinding? = null
+    private val binding get() = _binding!!
+    private val sRandom: Random = Random()
     private var mBottleImageView: ImageView? = null
+    private var wheelImage:ImageView? = null
     private var lastAngle = -1
-
+    var scoreP: TextView? = null
+    var scoreRes = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,44 +31,44 @@ class SpinFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spin, container, false)
+    ): View {
+        _binding = FragmentSpinBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)=with(binding) {
         super.onViewCreated(view, savedInstanceState)
-        mBottleImageView = activity?.findViewById<ImageView>(R.id.imageview_bottle)
-
-        mBottleImageView!!.setOnClickListener(View.OnClickListener { spinBottle() })
+        wheelImage = imageviewWheel
+        imageviewWheel.setOnClickListener{spinBottle()}
+      scoreP =   score
+//        mBottleImageView = activity?.findViewById<ImageView>(R.id.imageview_wheel)
+//        score = activity?.findViewById(R.id.score)
+//        mBottleImageView!!.setOnClickListener(View.OnClickListener { spinBottle() })
     }
     private fun spinBottle() {
 
         val angle = sRandom.nextInt(3600 - 360) + 360
         // Центр вращения
-        val pivotX = (mBottleImageView!!.width / 2).toFloat()
-        val pivotY = (mBottleImageView!!.height / 2).toFloat()
+        val pivotX = (wheelImage!!.width / 2).toFloat()
+        val pivotY = (wheelImage!!.height / 2).toFloat()
         val animation: Animation = RotateAnimation(
             (if (lastAngle == -1) 0 else lastAngle).toFloat(),
             angle.toFloat(), pivotX, pivotY
         )
         lastAngle = angle
+        scoreRes += angle
+        Log.d("score", scoreRes.toString())
+        scoreP?.text = scoreRes.toString()
         animation.duration = 2500
         animation.fillAfter = true
-        mBottleImageView!!.startAnimation(animation)
+        wheelImage!!.startAnimation(animation)
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance() = SpinFragment()
     }
 }
